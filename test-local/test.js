fa4fcc2b3993f80d1e7a7a08d004cb91c6516ce3
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-08-21 20:57:29 +0200

    test: local tests

---
+++ test-local/test.js
@@ -0,0 +1,18 @@
+import { Octokit } from "@octokit/rest";
+import script from "../action.js"
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const octokit = new Octokit({
+    auth: process.env.GH_TOKEN,
+});
+console.log(script)
+
+script({
+    github: octokit,
+    project: process.env.PROJECT,
+    scheduleFieldName: process.env.SCHEDULE_FIELD_NAME,
+    statusFieldName: process.env.STATUS_FIELD_NAME,
+    scheduleStateName: process.env.SCHEDULE_STATE_NAME,
+    todoStateName: process.env.TODO_STATE_NAME,
+}).catch(console.error)
