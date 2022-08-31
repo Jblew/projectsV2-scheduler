82e956c7555410f3ccdf649f61a25ade0e84f785
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-08-31 19:02:58 +0200

    perf: diagnostic info on action inputs

--- src/action.js
+++ src/action.js
@@ -5,6 +5,7 @@ module.exports = async ({
     github, log,
     projectURL, statusFieldName, scheduleFieldName, scheduleStateName, todoStateName,
 }) => {
+    log(`Running projectsv2 scheduler with params: ${prettyJSON({ projectURL, statusFieldName, scheduleFieldName, scheduleStateName, todoStateName, })}`)
     const graphql = new GraphQLAdapter({ github })
     const project = await fetchSchedulableProject({
         graphql, url: projectURL, statusFieldName, scheduleFieldName,
@@ -12,7 +13,6 @@ module.exports = async ({
     })
     const scheduledItems = await project.getScheduledItems()
     log(`Scheduled items (${scheduledItems.length}): ${prettyJSON(scheduledItems)}`)
-
     for (const item of scheduledItems) {
         if (Date.now() > item.date.getTime()) {
             log(`Descheduling item: ${prettyJSON(item)}`)
